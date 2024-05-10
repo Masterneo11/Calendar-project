556515d68c67849e953a3d6572116892269c861a
Mathew Zamacona <mateoszamacona@icloud.com>
2024-05-10 14:06:19 -0600

    rename files

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI, HTTPException, Depends, Response
 from schemas import CalendarEvent, Create_Calendar_Event
 from googleapiclient.discovery import build
 from typing import List
-import quickstart2
+import google_service
 
-from quickstart2 import delete_event
+from google_service import delete_event
 from database import get_db
 from models import Users
 from sqlmodel import Session, select
@@ -21,22 +21,22 @@ app = FastAPI()
 
 @app.get("/events")
 async def get_calendar_events():
-    service = quickstart2.get_google_service()
-    return quickstart2.get_events(service)
+    service = google_service.get_google_service()
+    return google_service.get_events(service)
 
 @app.post("/events", response_model=Create_Calendar_Event)
 async def create_event(event: Create_Calendar_Event, db: Session = Depends(get_db)):
-    service = quickstart2.get_google_service()
-    event_id, _ = quickstart2.create_event(service, event)
+    service = google_service.get_google_service()
+    event_id, _ = google_service.create_event(service, event)
     event.eventId = event_id
     return event
 
 @app.delete("/events/{event_id}")
 async def delete_calendar_event(event_id: str):
     # Assuming you have defined 'service' elsewhere in your code
-    service = quickstart2.get_google_service()
+    service = google_service.get_google_service()
     try:
-        quickstart2.delete_event(service, event_id)
+        google_service.delete_event(service, event_id)
         return {"message": f"Event with ID {event_id} deleted successfully."}
     except HttpError as error:
         raise HTTPException(status_code=500, detail=f"An error occurred while deleting the event: {error}")
@@ -44,16 +44,16 @@ async def delete_calendar_event(event_id: str):
 
 @app.put("/events/{id}")
 async def update_event(id: str, event):
-    service = quickstart2.get_google_service()
-    quickstart2.update_event( event, id, service)
+    service = google_service.get_google_service()
+    google_service.update_event( event, id, service)
 
 
 @app.get("/flitered_events")
 async def get_filtered_events() -> List[CalendarEvent]:
     try:
-        service = quickstart2.get_google_service()
-        events = quickstart2.get_events(service)
-        filtered_events = quickstart2.filter_events(events)
+        service = google_service.get_google_service()
+        events = google_service.get_events(service)
+        filtered_events = google_service.filter_events(events)
         return filtered_events
     except HttpError as error:
     
